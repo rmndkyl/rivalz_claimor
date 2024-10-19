b4a9d24510a19f376be92cff3d8ad6632d9ec1c7
Mandaa <99118244+rmndkyl@users.noreply.github.com>
2024-10-19 18:03:34 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,33 @@
+# Setting up Rivalz Fragment Auto-Claim
+### Update packages
+<code>apt-get update -y; apt upgrade -y</code>
+### Install the screen utility
+<code>sudo apt-get install screen</code>
+### Open a screen session
+<code>screen -S rivalz_auto_claim</code>
+### Clone the script
+<code>git clone https://github.com/rmndkyl/rivalz_claimor.git</code>
+### Navigate to the script folder
+<code>cd rivalz_claimor</code>
+### Download and install Python
+<code>add-apt-repository -y ppa:deadsnakes/ppa</code></br>
+<code>apt-get install python3.12 -y</code></br>
+<code>apt install python3-pip -y</code></br>
+### Install dependencies
+<code>pip install -r requirements.txt</code>
+### Run the script
+<code>python app.py</code></br>
+![image](https://github.com/user-attachments/assets/4c8f11ea-62a9-487c-bacd-6f4bec423f90)
+
+# What it does::
+1. Claims to your wallet (you will need to enter your seed phrase or privatekey)</br>
+2. Claims to a newly created wallet</br>
+IMPORTANT!!!</br>
+Don’t forget to refill your wallet with tokens from the faucet occasionally so you have gas for transactions.</br>
+The cooldown between batches of claims is 12 hours and 1 minute.</br>
+The delay between individual claims ranges from 5 to 15 seconds.
+
+## To exit the screen session without stopping the script, press ctrl+a, then D.
+
+### https://t.me/layerairdrop - Group
+### https://t.me/https://t.me/+UgQeEnnWrodiNTI1 - Chat
---
+++ app.py
@@ -0,0 +1,127 @@
+import random
+import time
+import eth_account.signers.local
+import web3
+from web3 import Web3, Account
+from config import RPC_URL, CONTRACT_ADDRESS, CONTRACT_METHOD, CHAIN_ID
+
+
+def get_logo():
+    print("""
+██╗░░░░░░█████╗░██╗░░░██╗███████╗██████╗░  ░█████╗░██╗██████╗░██████╗░██████╗░░█████╗░██████╗░
+██║░░░░░██╔══██╗╚██╗░██╔╝██╔════╝██╔══██╗  ██╔══██╗██║██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗
+██║░░░░░███████║░╚████╔╝░█████╗░░██████╔╝  ███████║██║██████╔╝██║░░██║██████╔╝██║░░██║██████╔╝
+██║░░░░░██╔══██║░░╚██╔╝░░██╔══╝░░██╔══██╗  ██╔══██║██║██╔══██╗██║░░██║██╔══██╗██║░░██║██╔═══╝░
+███████╗██║░░██║░░░██║░░░███████╗██║░░██║  ██║░░██║██║██║░░██║██████╔╝██║░░██║╚█████╔╝██║░░░░░
+╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝  ╚═╝░░╚═╝╚═╝╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝░╚════╝░╚═╝░░░░░
+Telegram community: https://t.me/layerairdrop
+""")
+
+
+def menu() -> int:
+    x = None
+    while not x:
+        print("""
+1) Enter your seed phrase
+2) Generate and use a new wallet
+3) Enter your private key
+        """)
+        x = input("-> ")
+    return int(x)
+
+
+def get_account_from_seed(w3: web3.Web3) -> eth_account.signers.local.LocalAccount:
+    w3.eth.account.enable_unaudited_hdwallet_features()
+    print("Enter your seed phrase:")
+    phrase = input("-> ")
+    try:
+        account = w3.eth.account.from_mnemonic(phrase)
+        print(f"Your wallet address: {account.address}")
+        return account
+    except Exception as e:
+        print(e)
+        main()
+
+
+def generate_account(w3: web3.Web3) -> eth_account.signers.local.LocalAccount:
+    w3.eth.account.enable_unaudited_hdwallet_features()
+    print("Generating a new wallet...")
+    account, mnemonic = w3.eth.account.create_with_mnemonic()
+    print(f"Your wallet address: {account.address}")
+    print(f"Your wallet seed phrase: {mnemonic}")
+    print(f"Your wallet private key: {account.key.hex()}")
+    return account
+
+
+def get_account_from_private_key(w3: web3.Web3) -> eth_account.signers.local.LocalAccount:
+    print("Enter your private key:")
+    private_key = input("-> ")
+    try:
+        account = w3.eth.account.from_key(private_key)
+        print(f"Your wallet address: {account.address}")
+        return account
+    except Exception as e:
+        print(f"Invalid private key: {e}")
+        main()
+
+
+def check_eth_balance(w3: web3.Web3, account: eth_account.signers.local.LocalAccount):
+    balance = w3.from_wei(w3.eth.get_balance(account.address), "ether")
+    print(f"Your wallet balance: {balance} ETH")
+    if balance < 0.00001:
+        print("Request some funds for your wallet using this link:")
+        print("https://rivalz2.hub.caldera.xyz/")
+        input("Then press Enter")
+    else:
+        input("Press Enter to start auto-claim")
+
+
+def claim_nft(w3: web3.Web3, account: eth_account.signers.local.LocalAccount):
+    nonce = w3.eth.get_transaction_count(account.address)
+    gas_price = w3.eth.gas_price
+    transaction = {
+        'to': CONTRACT_ADDRESS,
+        'value': 0,
+        'data': CONTRACT_METHOD,
+        'chainId': CHAIN_ID,
+        'nonce': nonce
+    }
+    try:
+        gas_limit = w3.eth.estimate_gas(transaction)
+        transaction['gas'] = gas_limit
+        transaction['gasPrice'] = gas_price
+        signed_txn = w3.eth.account.sign_transaction(transaction, account.key)
+        tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+        print(f"NFT claim hash: {tx_hash.hex()}")
+    except Exception as e:
+        print(f"Transaction error: {e}")
+
+
+def main() -> None:
+    w3 = Web3(Web3.HTTPProvider(RPC_URL))
+    if w3.is_connected():
+        choice = menu()
+        if choice == 1:
+            account = get_account_from_seed(w3)
+        elif choice == 2:
+            account = generate_account(w3)
+        elif choice == 3:
+            account = get_account_from_private_key(w3)
+        check_eth_balance(w3, account)
+
+        while True:
+            print("Starting claim process")
+            for i in range(20):
+                claim_nft(w3, account)
+                print(f"{i + 1} claim request sent successfully")
+                time.sleep(random.randint(5, 15))
+            print("Waiting 12 hours cooldown")
+            time.sleep(43260)
+    else:
+        print("Web3 connection failed!")
+        print("Check your internet connection")
+
+
+if __name__ == "__main__":
+    get_logo()
+    main()
